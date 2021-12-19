466b24f41fb2a7ec7df09c23b6aee637b7a69bc4
WINDOWS-TPIJSP4\User <mmatiullahsyed@gmail.com>
2021-12-19 17:11:50 +0500

    h

--- Project/Project/Startup.cs
+++ Project/Project/Startup.cs
@@ -17,7 +17,7 @@ namespace Project
         {
             Configuration = configuration;
         }
-
+        //highiih
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
